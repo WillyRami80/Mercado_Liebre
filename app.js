046c3924e14b6c8b1da59b90eb2408023d6360dd
WillyRami80 <waramirez80@gmail.com>
2023-05-09 23:57:13 -0500

    Tratando de subirlo

--- app.js
+++ app.js
@@ -2,10 +2,22 @@ const express = require ('express');
 const app = express();
 
 //app.use ('/static', express.static(__dirname + '/public'));
-app.use (express.static('public'))
+app.use (express.static('public'));
+
 app.listen(process.env.PORT || 3000, function() {
-    console.log("Servidor corriendo");
+    console.log("Servidor corriendo")
 })
 app.get('/',(req, res) => {
     res.sendFile(__dirname + '/views/home.html') 
+})
+app.get('/login',(req,res) => {
+    res.sendFile(__dirname + '/views/login.html')
+})
+
+app.post('/register',(req,res) => {
+    res.sendFile(__dirname + '/views/home.html')
+})
+
+app.post('/login',(req,res) => {
+    res.sendFile(__dirname + '/views/home.html')
 })
